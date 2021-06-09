c2c76dcddcf0c20c2f7e617e3bd15e16c4459678
ninsurin <85234584+ninsurin@users.noreply.github.com>
2021-06-09 18:03:13 +0700

    Add files via upload

---
+++ components/todos/todo.component.ts
@@ -0,0 +1,47 @@
+import { Component,OnInit} from "@angular/core";
+
+import{Todo} from './../../models/Todo'
+@Component({
+    selector:'app-todos',
+    templateUrl:'./todo.component.html',
+    styleUrls:['./todo.component.css']
+})
+export class TodoComponent implements OnInit{
+
+    todos!:Todo[];
+
+    inputTodo:string  = "";
+    constructor() {}
+
+    ngOnInit(): void{
+        this.todos = [
+            {
+                content: 'Frist Todo',
+                completed:false
+            },
+            {
+                content:'Second todo',
+                completed:false
+            }
+
+        ]
+    }
+
+    toggleDone(id:number){
+        this.todos.map((v, i) =>{
+            if (i == id) v.completed = !v.completed;
+
+            return v;
+        })
+    }
+    deleteTodo (id:number){
+        this.todos = this.todos.filter((v,i) => i !==id);
+    }
+    addTodo (){
+        this.todos.push({
+            content:this.inputTodo,
+            completed: false
+        });
+        this.inputTodo = ""
+    }
+}
